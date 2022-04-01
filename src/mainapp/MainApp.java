360f9db91a515c9d209cf4b0b99aa6030afd4db6
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-04-01 10:50:47 +0200

    Segunda version

--- src/mainapp/MainApp.java
+++ src/mainapp/MainApp.java
@@ -1,10 +1,23 @@
 package mainapp;
 
+/**
+ * Clase principal
+ * 
+ * @author adiaz
+ *
+ */
 public class MainApp {
 
+	/**
+	 * Metodo principal
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("Hola Mundo");
+		
+		System.out.println("Hola desarrollador");
 	}
 
 }
